7f812191d8dbdfb6d1f430247ec57c060e45c526
OTTDigitalLearning <50411089+OTTDigitalLearning@users.noreply.github.com>
2019-05-09 11:36:06 +0300

    Website

---
+++ rlwebdev.github.io/deloitte_demo/lib/common.js
@@ -0,0 +1,14 @@
+$(function() {
+        
+        $('[data-toggle="popover"]').popover();
+        $('[data-toggle="tooltip"]').tooltip();
+        
+
+        //http://veno.es/venobox/
+        $(".venobox").venobox();
+        /*<a class="venobox" data-vbtype="iframe" href="http://www.veno.es">Open Iframe</a>
+        <a class="venobox" data-vbtype="inline" title="My Description" href="#inline">Open inline content</a>
+        <a class="venobox" data-vbtype="ajax" href="ajax-call.php">Retrieve data via Ajax</a>
+        <a class="venobox" data-vbtype="video" href="http://youtu.be/d85gkOXeXG4">YouYbe</a>
+        <a class="venobox" data-vbtype="video" data-autoplay="true" href="http://vimeo.com/75976293">Vimeo</a>*/
+});
